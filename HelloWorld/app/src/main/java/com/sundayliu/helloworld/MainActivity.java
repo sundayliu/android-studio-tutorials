83b2e1e118b2f343a235ad1efe876d60892b5185
chameleonliu <martinkro@aliyun.com>
2016-06-24 17:38:06 +0800

    delete temp file

--- HelloWorld/app/src/main/java/com/sundayliu/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/sundayliu/helloworld/MainActivity.java
@@ -10,4 +10,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public static native String test();
+
+    static{
+        System.loadLibrary("helloworld");
+    }
 }
--- HelloWorld/app/src/main/jni/main.cpp
+++ HelloWorld/app/src/main/jni/main.cpp
@@ -1,6 +1,46 @@
 #include <jni.h>
+#include <stdlib.h>
+#include <android/log.h>
 
-jint JNI_OnLoad(JavaVM* jvm, void*)
+#include <string>
+
+jstring stringFromJNI(JNIEnv* env, jclass* clz)
 {
-    return 0;
+    __android_log_print(ANDROID_LOG_DEBUG, "debug", "Hello, NDKDemo!!!");
+    return env->NewStringUTF("Hello, C++ STL!!!");
+}
+
+const char* JNIREG_CLASS_NAME = "com/sundayliu/helloworld/MainActivity";
+static JNINativeMethod g_native_methonds[] =
+        {
+                {"test", "()Ljava/lang/String;", (void*)stringFromJNI}
+        };
+
+
+jint JNI_OnLoad(JavaVM* jvm, void* reserved)
+{
+    jint result = JNI_ERR;
+    JNIEnv* env = NULL;
+    if (jvm->GetEnv((void**)&env, JNI_VERSION_1_4) == JNI_OK)
+    {
+        jclass clz = env->FindClass(JNIREG_CLASS_NAME);
+        if (clz != NULL)
+        {
+            if (env->RegisterNatives(clz, g_native_methonds, sizeof(g_native_methonds) / sizeof(g_native_methonds[0])) >= 0)
+            {
+                result = JNI_VERSION_1_4;
+            }
+        }
+    }
+    return result;
+}
+
+// test
+namespace test
+{
+    void test_stl()
+    {
+        std::string hello = "Hello, C++ STL!";
+        __android_log_print(ANDROID_LOG_DEBUG, "debug", hello.c_str());
+    }
 }
