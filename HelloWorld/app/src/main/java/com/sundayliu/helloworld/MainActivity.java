f57656a1660382184d25f030eeadbb5c1d7beef8
chameleonliu <martinkro@aliyun.com>
2016-06-27 10:31:24 +0800

    ndk build

--- HelloWorld/app/src/main/java/com/sundayliu/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/sundayliu/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.sundayliu.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String strTest = test();
+        TextView v = (TextView)findViewById(R.id.tv_hello);
+        v.setText(strTest);
+
     }
 
     public static native String test();
